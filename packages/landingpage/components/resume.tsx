8bee58c31aed276af01f2654d1a2ceaef6ce8e8f
Joshua Herman <joshsimages@gmail.com>
2020-07-16 18:21:35 -0600

    Updated resume.tsx
    Add my resume and update packages

--- packages/landingpage/components/resume.tsx
+++ packages/landingpage/components/resume.tsx
@@ -1,11 +1,57 @@
-// import ResumeMD from '../public/docs/resume.md'
-
-function Resume () {
+export default function Resume () {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div id='target'>
+      <h1 className='rName'>Joshua J. Herman</h1>
+      <h4 className='rContact'>Gillette, WY - (307)-689-0503 - joshsimages@gmail.com - <a className='rLink' href='https://www.joshuaherman.tech' target='_blank' rel='noopener noreferrer'>www.joshuaherman.tech</a></h4>
+      <div className='rDivider' />
+      <p><strong className='rDetail'>Background in management, teamwork, automation, and programming.</strong></p>
+      <div className='rDivider' />
+      <h2 className='rAction'>Experience</h2>
+      <p><strong className='rPosition'>Automation Technician, Pro-Maac Systems, INC</strong><em> (August 2019 – Current)</em></p>
+      <p>Automated oil and gas sites using wireless and hardwired end devices connected to RTUs/PLCs for SCADA systems.</p>
+      <ul>
+        <li className='rList'>Troubleshooting of RS-232, RS-485, Ethernet networks</li>
+        <li className='rList'>Reading and applying complex documentation for site installs</li>
+        <li className='rList'>Building spreadsheets for clients and employees for site networks configurations</li>
+      </ul>
+      <p><strong className='rPosition'>Financial Service Representative, First Interstate Bank</strong><em> (March 2016 – August 2019)</em></p>
+      <p>My position was a universal banker who helped clients open consumer and business accounts other duties were helping them with all banking information.</p>
+      <ul>
+        <li className='rList'>Worked with management on a special task team to improve corporate efficiency and teamwork</li>
+        <li className='rList'>Directing clients to the best financial products</li>
+        <li className='rList'>Dealing with large amounts of money and transactions</li>
+      </ul>
+      <p><strong className='rPosition'>Apprentice Electrician, N.E.W. Electric</strong><em> (June 2014 – May 2015)</em></p>
+      <p>Worked with journeyman electricians installing and repairing electrical equipment for the energy sector.</p>
+      <ul>
+        <li className='rList'>Installing and repairing electrical equipment ranging from VFDs to lighting</li>
+        <li className='rList'>Troubleshooting electrical issues both high and low voltage</li>
+        <li className='rList'>Reading technical diagrams for installs</li>
+      </ul>
+      <p><strong className='rPosition'>Shop Foreman, Tri-State Industries</strong><em> (June 2005 – May 2014)</em></p>
+      <p>Managed an entire shop of 10-20 people fabricating and installing metering units for coal bed methane and oil.</p>
+      <ul>
+        <li className='rList'>Translated project ideas to man hours required to complete</li>
+        <li className='rList'>Was responsible for the training and management of new hires</li>
+        <li className='rList'>Made sure projects were completed before deadlines</li>
+      </ul>
+      <div className='rDivider' />
+      <h2 className='rAction'>Education</h2>
+      <p><strong className='rPosition'>Associates in Applied Sciences, Gillette College</strong><em> (Graduating Fall 2020)</em></p>
+      <ul>
+        <li className='rList'>Major in Networking &amp; Integrated Systems</li>
+        <li className='rList'>Presidential honors</li>
+      </ul>
+      <div className='rDivider' />
+      <h2 className='rAction'>Skills</h2>
+      <ul>
+        <li><strong className='rPosition'>Networking</strong><em> (Cisco, Ubiquiti, Pfsense, SignalFire, FreeWave)</em></li>
+        <li><strong className='rPosition'>Programming</strong><em> (Docker, Ansible, Git, Bash, Python, JavaScript, YAML, HTML, CSS)</em></li>
+        <li><strong className='rPosition'>Automation</strong><em> (RS-485, RS-232, PLCs, SCADA)</em></li>
+        <li><strong className='rPosition'>Operating Systems</strong><em> (Windows, Debian, and BSD)</em></li>
+        <li><strong className='rPosition'>Fabrication</strong><em> (Electrical, Welding, Pipe Fitting)</em></li>
+        <li><strong className='rPosition'>Microsoft</strong><em> (Word, Excel, PowerPoint, Outlook)</em></li>
+      </ul>
     </div>
   )
 }
-
-export default Resume
--- packages/landingpage/next.config.js
+++ packages/landingpage/next.config.js
@@ -1,3 +1,4 @@
+// next.config.js
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
